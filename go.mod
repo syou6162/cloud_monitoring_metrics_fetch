cd8c99a985c91432b6bdf9dfd9a8e4889751e8b0
Yasuhisa Yoshida <syou6162@gmail.com>
2022-02-01 10:59:45 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/syou6162/cloud_monitoring_metrics_fetch
+
+go 1.16
+
+require (
+	cloud.google.com/go/monitoring v1.2.0
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/api v0.66.0
+	google.golang.org/genproto v0.0.0-20220126215142-9970aeb2e350
+)
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	monitoring "cloud.google.com/go/monitoring/apiv3"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"google.golang.org/api/iterator"
+	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
+)
+
+type KeyValue struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type Point struct {
+	Timestamp time.Time  `json:"timestamp"`
+	Labels    []KeyValue `json:"labels"`
+	Value     float64    `json:"value"`
+}
+
+func convertKeyValuePairs(labels map[string]string) []KeyValue {
+	kvs := make([]KeyValue, 0)
+	for k, v := range labels {
+		kvs = append(kvs, KeyValue{Key: k, Value: v})
+	}
+	return kvs
+}
+
+func readAndPrintTimeSeriesFields(
+	ctx context.Context,
+	projectID string,
+	metricType string,
+	resourceType string,
+	startTime time.Time,
+	endTime time.Time,
+) error {
+	client, err := monitoring.NewMetricClient(ctx)
+	if err != nil {
+		return fmt.Errorf("NewMetricClient: %v", err)
+	}
+	defer client.Close()
+
+	req := &monitoringpb.ListTimeSeriesRequest{
+		Name:   "projects/" + projectID,
+		Filter: fmt.Sprintf("metric.type=\"%s\" resource.type=\"%s\"", metricType, resourceType),
+		Interval: &monitoringpb.TimeInterval{
+			StartTime: &timestamp.Timestamp{
+				Seconds: startTime.Unix(),
+			},
+			EndTime: &timestamp.Timestamp{
+				Seconds: endTime.Unix(),
+			},
+		},
+		View: monitoringpb.ListTimeSeriesRequest_FULL,
+	}
+
+	it := client.ListTimeSeries(ctx, req)
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("could not read time series value: %v", err)
+		}
+		labels := convertKeyValuePairs(resp.GetMetric().GetLabels())
+		points := make([]Point, 0)
+		for _, p := range resp.GetPoints() {
+			timestamp := p.GetInterval().StartTime.AsTime()
+			val := p.GetValue().GetDoubleValue()
+			points = append(points, Point{Timestamp: timestamp, Labels: labels, Value: val})
+		}
+
+		outputJson, err := json.Marshal(&points)
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(outputJson))
+	}
+	return nil
+}
+
+func main() {
+	var (
+		project      = flag.String("project", "", "GCP project")
+		metricType   = flag.String("metricType", "", "Type of metric")
+		resourceType = flag.String("resourceType", "", "Type of resource")
+		start        = flag.Int64("start", time.Now().Add(time.Duration(-10)*time.Minute).Unix(), "Start time (unix time)")
+		end          = flag.Int64("end", time.Now().Unix(), "End time (unix time)")
+	)
+	flag.Parse()
+	ctx := context.Background()
+
+	if err := readAndPrintTimeSeriesFields(ctx, *project, *metricType, *resourceType, time.Unix(*start, 0), time.Unix(*end, 0)); err != nil {
+		log.Fatal(err)
+	}
+}
